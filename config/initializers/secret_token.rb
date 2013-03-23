9ae4a12f14fb3ddf073018fa19fb6e13cd85677b
Alex Leschenko <leschenko.al@gmail.com>
2013-03-23 19:30:15 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExampleElasticsearchAutocomplete::Application.config.secret_token = '919aa5ec115e37059aa32efc35bde7a686e11730304d49ed5d3565597ab9c5d9e5bbc0ed1e648edae589f6edc01fc3ea09fab8ca83eadd970bdc1f4d8a59b4da'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ExampleElasticsearchAutocomplete::Application.config.session_store :cookie_store, key: '_example_elasticsearch_autocomplete_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ExampleElasticsearchAutocomplete::Application.config.session_store :active_record_store
